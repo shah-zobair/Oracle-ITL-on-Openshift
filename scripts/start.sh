4284544be0524688aff01f86cc13c6cd04f4c590
Shah Zobair <shah-zobair@users.noreply.github.com>
2016-10-17 22:07:22 -0400

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
 
-#Run run.sh
-#
-#/run.sh
 
 ################
 # Start Platform Service
 #
 echo "Starting Platform Services"
 /apps/opt/weblogic/endeca/PlatformServices/11.1.0/tools/server/bin/startup.sh &
-echo "Done PS"
+echo "Started PS"
 
 ################
 # Start Tools and Framework
 #
 echo "Starting Tools and Framework"
 /apps/opt/weblogic/endeca/ToolsAndFrameworks/11.1.0/server/bin/startup.sh &
-echo "Done T&F"
+echo "Started T&F"
+
+################
+# Start CAS
+#
+echo "Starting CAS"
+/apps/opt/weblogic/endeca/CAS/11.1.0/bin/cas-service.sh &
+echo "Started CAS"
 
 ###############
 # Deploy Search Application
 #
 # Create Response file for the wizard
-echo '' > /search.rsp
-echo 'Y' >> /search.rsp
-echo 'Search' >> /search.rsp
-echo '/apps/opt/weblogic/endeca/apps' >> /search.rsp
-echo '8888' >> /search.rsp
-echo '8006' >> /search.rsp
-echo '17000' >> /search.rsp
-echo '17002' >> /search.rsp
-echo '17010' >> /search.rsp
-echo '' >> /search.rsp
-echo '/apps/opt/weblogic/endeca/ToolsAndFrameworks/11.1.0/deployment_template/lib/../../server/workspace/credential_store/jps-config.xml' >> /search.rsp
-echo '/apps/opt/weblogic/endeca/ToolsAndFrameworks/11.1.0/deployment_template/lib/../../server/workspace/state/repository' >> /search.rsp
-echo '' >> /search.rsp
+echo '' > /tmp/search.rsp
+echo 'Y' >> /tmp/search.rsp
+echo 'Search' >> /tmp/search.rsp
+echo '/apps/opt/weblogic/endeca/apps' >> /tmp/search.rsp
+echo '8888' >> /tmp/search.rsp
+echo '8006' >> /tmp/search.rsp
+echo '17000' >> /tmp/search.rsp
+echo '17002' >> /tmp/search.rsp
+echo '17010' >> /tmp/search.rsp
+echo '' >> /tmp/search.rsp
+echo '/apps/opt/weblogic/endeca/ToolsAndFrameworks/11.1.0/deployment_template/lib/../../server/workspace/credential_store/jps-config.xml' >> /tmp/search.rsp
+echo '/apps/opt/weblogic/endeca/ToolsAndFrameworks/11.1.0/deployment_template/lib/../../server/workspace/state/repository' >> /tmp/search.rsp
+echo '' >> /tmp/search.rsp
 
 # Run the Wizard
-/apps/opt/weblogic/endeca/ToolsAndFrameworks/11.1.0/deployment_template/bin/deploy.sh --app /apps/opt/weblogic/endeca/ToolsAndFrameworks/11.1.0/reference/discover-data/deploy.xml < /search.rsp
+echo "Running the Search App Wizard now"
+
+/apps/opt/weblogic/endeca/ToolsAndFrameworks/11.1.0/deployment_template/bin/deploy.sh --app /apps/opt/weblogic/endeca/ToolsAndFrameworks/11.1.0/reference/discover-data/deploy.xml < /tmp/search.rsp
+
+# Initialize the App
+#echo "Initializing the App"
+#/apps/opt/weblogic/endeca/apps/Search/control/initialize_services.sh
